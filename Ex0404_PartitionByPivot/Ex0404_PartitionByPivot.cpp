c73735a0234496be4611f0eadbb90700268c77c0
nansu0425 <nansu0425@gmail.com>
2024-09-20 15:54:25 +0900

    Ex0404 완료

--- Ex0404_PartitionByPivot/Ex0404_PartitionByPivot.cpp
+++ Ex0404_PartitionByPivot/Ex0404_PartitionByPivot.cpp
@@ -11,20 +11,27 @@ void Print(vector<int>& arr)
 
 int main()
 {
-	vector<int> arr = { 5, 2, 7, 3, 8, 5, 6, 4 };
+	//vector<int> arr = { 5, 2, 7, 3, 8, 5, 6, 4 };
 	//vector<int> arr = { 2, 8, 7, 1, 3, 5, 6, 4 };
 	//vector<int> arr = { 9, 8, 7, 6, 4, 3, 2, 1, 5 };
-	//vector<int> arr = { 5, 2, 7, 3, 4, 4, 6, 4 };
+	vector<int> arr = { 5, 2, 7, 3, 4, 4, 6, 4 };
 
 	int lo = 0;  // 첫 인덱스
 	int hi = arr.size() - 1;  // 마지막 인덱스
 	int x = arr[hi]; // 분할 기준으로 사용하는 pivot 4
 
-	int i = lo - 1; // pivot보다 것들중 가장 큰 인덱스
+	int i = lo - 1; // pivot 값 이하인 원소 중 가장 큰 인덱스
 
 	Print(arr);
 
-	// TODO:
+	for (int arrIdx = 0; arrIdx < hi; ++arrIdx)
+	{
+		if (arr[arrIdx] <= x)
+		{
+			++i;
+			std::swap(arr[i], arr[arrIdx]);
+		}
+	}
 
 	swap(arr[i + 1], arr[hi]);
 	Print(arr);
