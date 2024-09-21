c3194405906414fc61680a5b06a40e4f2169865d
nansu0425 <nansu0425@gmail.com>
2024-09-21 22:54:10 +0900

    Ex0603 완료

--- Ex0603_SortingWords/Ex0603_SortingWords.cpp
+++ Ex0603_SortingWords/Ex0603_SortingWords.cpp
@@ -7,6 +7,8 @@
 #include <numeric>
 #include <iomanip>
 #include <string>
+#include <utility>
+
 using namespace std;
 
 template<typename T>
@@ -17,17 +19,42 @@ void Print(vector<T>& arr)
 	cout << endl;
 }
 
-void CountingSort(vector<string>& arr, int k, int d)
+void CountingSort(vector<string>& arr, int k, int charIdx)
 {
-	// vector<string> temp = arr; // 복사
+	vector<string> oldArr = arr; // 복사
 	// std::fill(arr.begin(), arr.end(), "   "); // 디버깅 편의
 
-	// TODO:
+	vector<size_t> counts(k);
+
+	// 문자의 개수 세기
+	for (const std::string word : oldArr)
+	{
+		++counts[word[charIdx] - 'A'];
+	}
+
+	// counts 원소로 arr의 인덱스를 계산할 수 있도록 수정
+	for (int countsIdx = 1; countsIdx < counts.size(); ++countsIdx)
+	{
+		counts[countsIdx] += counts[countsIdx - 1];
+	}
+
+	// charIdx의 문자 기준으로 oldArr를 정렬하여 arr로 복사
+	for (int oldArrIdx = oldArr.size() - 1; oldArrIdx >= 0; --oldArrIdx)
+	{
+		int countsIdx = oldArr[oldArrIdx][charIdx] - 'A';
+
+		--counts[countsIdx];
+		arr[counts[countsIdx]] = oldArr[oldArrIdx];
+	}
+
 }
 
 void RadixSort(vector<string>& arr)
 {
-	// TODO:
+	for (int charIdx = arr[0].size() - 1; charIdx >= 0; --charIdx)
+	{
+		CountingSort(arr, 'Z' - 'A' + 1, charIdx);
+	}
 }
 
 int main()
