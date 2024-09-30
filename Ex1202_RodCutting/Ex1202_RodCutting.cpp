dd253a0ab575dfb5b0e0ef81a82b6e6c19ad05dc
nansu0425 <nansu0425@gmail.com>
2024-09-30 22:49:47 +0900

    Ex1202 완료

--- Ex1202_RodCutting/Ex1202_RodCutting.cpp
+++ Ex1202_RodCutting/Ex1202_RodCutting.cpp
@@ -13,9 +13,24 @@ int RecurCutRod(const vector<int>& prices, int length)
 
 	int max_price = numeric_limits<int>::min();
 
-	for (int i = 1; i <= length; i++)
+	for (int cutLength = 1; cutLength <= length; cutLength++)
 	{
-		// TODO:
+		int price = 0;
+		int cutLengthPrice = (cutLength == length) ? prices[length] : RecurCutRod(prices, cutLength);
+		int leftLength = length;
+
+		while (leftLength - cutLength >= 0)
+		{
+			leftLength -= cutLength;
+			price += cutLengthPrice;
+		}
+
+		if (leftLength > 0)
+		{
+			price += RecurCutRod(prices, leftLength);
+		}
+
+		max_price = std::max(max_price, price);
 	}
 
 	return max_price;
@@ -26,7 +41,25 @@ int MemoizedCutRodHelper(const vector<int>& prices, int length, vector<int>& mem
 	if (memo[length] >= 0)
 		return memo[length];
 
-	// TODO:
+	for (int cutLength = 1; cutLength <= length; ++cutLength)
+	{
+		int price = 0;
+		int cutLengthPrice = (cutLength == length) ? prices[length] : MemoizedCutRodHelper(prices, cutLength, memo);
+		int leftLength = length;
+
+		while (leftLength - cutLength >= 0)
+		{
+			leftLength -= cutLength;
+			price += cutLengthPrice;
+		}
+
+		if (leftLength > 0)
+		{
+			price += MemoizedCutRodHelper(prices, leftLength, memo);
+		}
+
+		memo[length] = std::max(memo[length], price);
+	}
 
 	for (auto& t : memo) cout << setw(3) << t; cout << endl;
 
@@ -46,11 +79,31 @@ int BottomUpCutRod(const vector<int>& prices, int length)
 	vector<int> table(length + 1, -1); // 가격은 음수가 될 수 없으니까 디버깅 편의를 위해 -1로 초기화
 	table[0] = 0; // length* prices[0];
 
-	for (int j = 1; j <= length; j++)
+	for (int targetLength = 1; targetLength <= length; targetLength++)
 	{
 		int max_price = numeric_limits<int>::min();
 
-		// TODO:
+		for (int cutLength = 1; cutLength <= targetLength; ++cutLength)
+		{
+			int price = 0;
+			int cutLengthPrice = (cutLength == targetLength) ? prices[targetLength] : table[cutLength];
+			int leftLength = targetLength;
+
+			while (leftLength - cutLength >= 0)
+			{
+				leftLength -= cutLength;
+				price += cutLengthPrice;
+			}
+
+			if (leftLength > 0)
+			{
+				price += table[leftLength];
+			}
+
+			max_price = std::max(max_price, price);
+		}
+
+		table[targetLength] = max_price;
 
 		for (auto& t : table) cout << setw(3) << t; cout << endl;
 	}
@@ -63,8 +116,66 @@ int ExtendedBottomUpCutRod(const vector<int>& prices, int length)
 {
 	vector<int> table(length + 1, -1); // 가격은 음수가 될 수 없으니까 디버깅 편의를 위해 -1로 초기화
 	table[0] = 0; // length* prices[0];
+	int finalSplitCnt = 0;
+	int finalCutLength = 0;
+	int finalLeftLength = 0;
 
-	// TODO:
+	for (int targetLength = 1; targetLength <= length; targetLength++)
+	{
+		int max_price = numeric_limits<int>::min();
+
+		for (int cutLength = 1; cutLength <= targetLength; ++cutLength)
+		{
+			int price = 0;
+			int cutLengthPrice = (cutLength == targetLength) ? prices[targetLength] : table[cutLength];
+			int leftLength = targetLength;
+			int splitCnt = 0;
+
+			while (leftLength - cutLength >= 0)
+			{
+				leftLength -= cutLength;
+				price += cutLengthPrice;
+
+				if (targetLength == length)
+				{
+					++splitCnt;
+				}
+			}
+
+			if (leftLength > 0)
+			{
+				price += table[leftLength];
+			}
+
+			if (max_price < price)
+			{
+				max_price = price;
+
+				if (targetLength == length)
+				{
+					finalSplitCnt = splitCnt;
+					finalCutLength = cutLength;
+					finalLeftLength = leftLength;
+				}
+			}
+		}
+
+		table[targetLength] = max_price;
+	}
+
+	std::cout << "Split: ";
+	
+	for (int splitCnt = 0; splitCnt < finalSplitCnt; ++splitCnt)
+	{
+		std::cout << finalCutLength << "(" << table[finalCutLength] << ") ";
+	}
+
+	if (finalLeftLength > 0)
+	{
+		std::cout << finalLeftLength << "(" << table[finalLeftLength] << ") ";
+	}
+
+	std::cout << std::endl;
 
 	return table[length];
 }
@@ -72,8 +183,8 @@ int ExtendedBottomUpCutRod(const vector<int>& prices, int length)
 int main()
 {
 	// Length:                  0  1  2  3  4   5   6   7   8   9  10
-	vector<int> price_table = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
-	//vector<int> price_table = { 0, 3, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+	// vector<int> price_table = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+	vector<int> price_table = { 0, 3, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
 
 	// 주의: price_table은 문제에서 주어진 조건입니다. 메모가 아닙니다.
 
