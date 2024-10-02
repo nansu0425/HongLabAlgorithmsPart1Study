b5c61df647d0ea35517b965efdb526789086ef81
nansu0425 <nansu0425@gmail.com>
2024-10-02 16:02:09 +0900

    Ex1205 완료

--- Ex1205_FloydWarshall/Ex1205_FloydWarshall.cpp
+++ Ex1205_FloydWarshall/Ex1205_FloydWarshall.cpp
@@ -5,12 +5,12 @@
 #include <deque>
 using namespace std;
 
-constexpr double kInf = numeric_limits<double>::infinity();
+constexpr double infDist = numeric_limits<double>::infinity();
 
 template<typename T>
 void Print(const vector<vector<T>>& dist)
 {
-	int V = dist.size();
+	size_t V = dist.size();
 
 	for (int i = 0; i < V; i++)
 	{
@@ -48,130 +48,146 @@ void PrintPaths(vector<int> prev)
 	}
 }
 
-void RepeatingBellmanFord(const vector<vector<double>>& graph)
+void RepeatingBellmanFord(const vector<vector<double>>& origDistTable)
 {
 	// 힌트: 벨먼-포드를 여러 번 반복하는 방식
 	cout << "Repeating Bellman-Ford" << endl;
 
-	vector<vector<double>> dist = graph; // 복사 후 업데이트 하는 방식
+	vector<vector<double>> distTable = origDistTable; // 복사 후 업데이트 하는 방식
 
-	int V = graph.size();
+	size_t numVertices = origDistTable.size();
+	vector<vector<int>> prevTable(numVertices, vector<int>(numVertices));
 
-	vector<vector<int>> prev(V, vector<int>(V));
-	for (int i = 0; i < V; i++)
-		for (int j = 0; j < V; j++)
+	for (int src = 0; src < numVertices; src++)
+	{
+		for (int dest = 0; dest < numVertices; dest++)
 		{
-			if (i == j || graph[i][j] == kInf)
-				prev[i][j] = -1;
-			else
-				prev[i][j] = i;
+			prevTable[src][dest] = ((src == dest) ||
+									(origDistTable[src][dest] == infDist))
+									? -1
+									: src;
 		}
+	}
 
 	cout << "dist 0" << endl;
-	Print(dist);
+	Print(distTable);
 	cout << "prev 0" << endl;
-	Print(prev);
+	Print(prevTable);
 
-	for (int s = 0; s < V; s++) // s는 시작 정점
+	for (int src = 0; src < numVertices; src++) // s는 시작 정점
 	{
-		for (int v = 1; v < V; v++)
+		for (int updateCnt = 1; updateCnt < numVertices; updateCnt++)
 		{
 			// CLRS 교재에서는 Floyd-Warshall을 구현할 때 사본을 만들어서 업데이트 방식이라서
 			// 여기서는 이해를 돕기 위해 Bellman-Ford도 비슷하게 구조를 바꿔봤습니다.
 			// 참고로, 위키피디아에서는 사본 없이 바로 덮어쓰는 in-place 방식입니다.
-			auto dist_k = dist;
-			auto prev_k = prev;
+			auto newDistTable = distTable;
+			auto newPrevTable = prevTable;
 
 			// Bellman-Ford에 인접 리스트 대신에 인접 행렬 사용
-			for (int i = 0; i < V; i++)
+			for (int prevDest = 0; prevDest < numVertices; prevDest++)
 			{
-				for (int j = 0; j < V; j++)
+				for (int dest = 0; dest < numVertices; dest++)
 				{
-					// TODO: grpah[][]와 dist[][]를 사용해서 dist_k와 prev_k 업데이트
+					const double newDist = distTable[src][prevDest] + origDistTable[prevDest][dest];
+
+					if (newDist < distTable[src][dest])
+					{
+						newDistTable[src][dest] = newDist;
+						newPrevTable[src][dest] = prevDest;
+					}
 				}
 			}
 
-			swap(dist_k, dist);
-			swap(prev_k, prev);
+			distTable = std::move(newDistTable);
+			prevTable = std::move(newPrevTable);
 
-			cout << "dist " << s + 1 << endl;
-			Print(dist);
-			cout << "prev " << s + 1 << endl;
-			Print(prev);
+			cout << "dist " << src + 1 << endl;
+			Print(distTable);
+			cout << "prev " << src + 1 << endl;
+			Print(prevTable);
 
-			cout << "Paths " << s + 1 << endl;
-			for (int i = 0; i < prev.size(); i++) {
+			cout << "Paths " << src + 1 << endl;
+			for (int i = 0; i < prevTable.size(); i++) {
 				cout << "From " << i << " to others:" << endl;
-				PrintPaths(prev[i]);
+				PrintPaths(prevTable[i]);
 			}
 		}
 	}
 }
 
-void FloydWarshall(const vector<vector<double>>& graph)
+void FloydWarshall(const vector<vector<double>>& origDistTable)
 {
 	cout << "FloydWarshall" << endl;
 
-	vector<vector<double>> dist = graph; // 복사 후 업데이트 하는 방식
+	vector<vector<double>> distTable = origDistTable; // 복사 후 업데이트 하는 방식
 
-	int V = graph.size();
+	const size_t numVertices = origDistTable.size();
+	vector<vector<int>> prevTable(numVertices, vector<int>(numVertices));
 
-	vector<vector<int>> prev(V, vector<int>(V));
-	for (int i = 0; i < V; i++)
-		for (int j = 0; j < V; j++)
+	for (int src = 0; src < numVertices; src++)
+	{
+		for (int dest = 0; dest < numVertices; dest++)
 		{
-			if (i == j || graph[i][j] == kInf)
-				prev[i][j] = -1;
-			else
-				prev[i][j] = i;
+			prevTable[src][dest] = ((src == dest) || 
+									(origDistTable[src][dest] == infDist)) 
+									? -1 
+									: src;
 		}
+	}
 
 	cout << "dist 0" << endl;
-	Print(dist);
+	Print(distTable);
 	cout << "prev 0" << endl;
-	Print(prev);
+	Print(prevTable);
 
-	for (int k = 0; k < V; k++)
+	for (int stopover = 0; stopover < numVertices; stopover++)
 	{
-		auto dist_k = dist;
-		auto prev_k = prev;
+		auto newDistTable = distTable;
+		auto newPrevTable = prevTable;
 
-		for (int i = 0; i < V; i++)
+		for (int src = 0; src < numVertices; src++)
 		{
-			for (int j = 0; j < V; j++)
+			for (int dest = 0; dest < numVertices; dest++)
 			{
-				// TODO: i -> j 와 i -> k -> j 비교해서 필요하면 업데이트
+				const double newDist = distTable[src][stopover] + distTable[stopover][dest];
+
+				if (newDist < distTable[src][dest])
+				{
+					newDistTable[src][dest] = newDist;
+					newPrevTable[src][dest] = prevTable[stopover][dest];
+				}
 			}
 		}
 
-		swap(dist_k, dist);
-		swap(prev_k, prev);
+		distTable = std::move(newDistTable);
+		prevTable = std::move(newPrevTable);
 
-		cout << "dist " << k + 1 << endl;
-		Print(dist);
-		cout << "prev " << k + 1 << endl;
-		Print(prev);
+		cout << "dist " << stopover + 1 << endl;
+		Print(distTable);
+		cout << "prev " << stopover + 1 << endl;
+		Print(prevTable);
 
-		cout << "Paths " << k + 1 << endl;
-		for (int i = 0; i < prev.size(); i++) {
+		cout << "Paths " << stopover + 1 << endl;
+		for (int i = 0; i < prevTable.size(); i++) {
 			cout << "From " << i << " to others:" << endl;
-			PrintPaths(prev[i]);
+			PrintPaths(prevTable[i]);
 		}
 	}
 }
 
 int main()
 {
-	vector<vector<double>> graph =
+	vector<vector<double>> distTable =
 	{
-		{0.0, 3.0, 8.0, kInf, -4.0},
-		{kInf, 0.0, kInf, 1.0, 7.0},
-		{kInf, 4.0, 0.0, kInf, kInf},
-		{2.0, kInf, -5.0, 0.0, kInf},
-		{kInf, kInf, kInf, 6.0, 0.0}
+		{0.0, 3.0, 8.0, infDist, -4.0},
+		{infDist, 0.0, infDist, 1.0, 7.0},
+		{infDist, 4.0, 0.0, infDist, infDist},
+		{2.0, infDist, -5.0, 0.0, infDist},
+		{infDist, infDist, infDist, 6.0, 0.0}
 	};
 
-	// RepeatingBellmanFord(graph);
+	// RepeatingBellmanFord(distTable);
 
-	FloydWarshall(graph);
+	FloydWarshall(distTable);
 }
