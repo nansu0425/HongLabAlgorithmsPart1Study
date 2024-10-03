dd9a438f6266fa06af15a83252f49246b2448304
nansu0425 <nansu0425@gmail.com>
2024-10-04 01:54:55 +0900

    Ex1304 완료

--- Ex1304_LargestIndependentSet/Ex1304_LargestIndependentSet.cpp
+++ Ex1304_LargestIndependentSet/Ex1304_LargestIndependentSet.cpp
@@ -9,60 +9,94 @@ struct Node
 public:
 	int value = 0;
 	int memo = 0; // <- Memoization에 사용
-	Node* left = nullptr;
-	Node* right = nullptr;
+	Node* pLeft = nullptr;
+	Node* pRight = nullptr;
 
 	// 꼭 이진 트리가 아니라 싸이클이 없는 트리 구조면 다 가능합니다.
 };
 
-int RecurLIS(Node* root)
+int RecurLIS(Node* pRoot)
 {
-	if (root == nullptr)
+	if (pRoot == nullptr)
+	{
 		return 0;
+	}
 
-	// Root 노드가 포함 되지 않는 경우: 자식들 포함 가능
-	int case1 = RecurLIS(root->left) + RecurLIS(root->right);
+	// case1: Root 노드가 포함 되지 않는 경우엔 자식들 포함 가능
+	int case1 = RecurLIS(pRoot->pLeft) + RecurLIS(pRoot->pRight);
 
-	// Root 노드가 포함되는 경우는 하나 건너서 자식들의 자식들만 포함 가능합니다.
+	// case2: Root 노드가 포함되는 경우는 하나 건너서 자식들의 자식들만 포함 가능
 	int case2 = 1;
-	if (root->left)
-		case2 += RecurLIS(root->left->left) + RecurLIS(root->left->right);
-	if (root->right)
-		case2 += RecurLIS(root->right->left) + RecurLIS(root->right->right);
+
+	if (pRoot->pLeft != nullptr)
+	{
+		case2 += RecurLIS(pRoot->pLeft->pLeft) + RecurLIS(pRoot->pLeft->pRight);
+	}
+
+	if (pRoot->pRight != nullptr)
+	{
+		case2 += RecurLIS(pRoot->pRight->pLeft) + RecurLIS(pRoot->pRight->pRight);
+	}
 
 	// 두 가지 경우 중 큰 경우 반환
 	return max(case1, case2);
 }
 
-int MemoLIS(Node* root)
+int MemoLIS(Node* pRoot)
 {
-	// TODO:
+	if (pRoot == nullptr)
+	{
+		return 0;
+	}
 
-	return 0;
+	if (pRoot->memo > 0)
+	{
+		return pRoot->memo;
+	}
+	
+	// case1: Root 노드가 포함 되지 않는 경우엔 자식들 포함 가능
+	int case1 = MemoLIS(pRoot->pLeft) + MemoLIS(pRoot->pRight);
+
+	// case2: Root 노드가 포함되는 경우는 하나 건너서 자식들의 자식들만 포함 가능
+	int case2 = 1;
+
+	if (pRoot->pLeft != nullptr)
+	{
+		case2 += MemoLIS(pRoot->pLeft->pLeft) + MemoLIS(pRoot->pLeft->pRight);
+	}
+
+	if (pRoot->pRight != nullptr)
+	{
+		case2 += MemoLIS(pRoot->pRight->pLeft) + MemoLIS(pRoot->pRight->pRight);
+	}
+
+	pRoot->memo = max(case1, case2);
+
+	return pRoot->memo;
 }
 
-Node* MakeNode(int data)
+Node* MakeNode(int value)
 {
 	Node* temp = new Node();
-	temp->value = data;
-	temp->left = temp->right = nullptr;
+	temp->value = value;
+	temp->pLeft = temp->pRight = nullptr;
 	return temp;
 }
 
 int main()
 {
 	// 이진 트리 만들기
-	Node* root = MakeNode(1);
-	root->left = MakeNode(2);
-	root->left->left = MakeNode(4);
-	root->left->right = MakeNode(5);
-	root->left->right->left = MakeNode(7);
-	root->left->right->right = MakeNode(8);
-	root->right = MakeNode(3);
-	root->right->right = MakeNode(6);
+	Node* pRoot = MakeNode(1);
+	pRoot->pLeft = MakeNode(2);
+	pRoot->pLeft->pLeft = MakeNode(4);
+	pRoot->pLeft->pRight = MakeNode(5);
+	pRoot->pLeft->pRight->pLeft = MakeNode(7);
+	pRoot->pLeft->pRight->pRight = MakeNode(8);
+	pRoot->pRight = MakeNode(3);
+	pRoot->pRight->pRight = MakeNode(6);
 
-	cout << RecurLIS(root) << endl;
-	cout << MemoLIS(root) << endl;
+	cout << RecurLIS(pRoot) << endl;
+	cout << MemoLIS(pRoot) << endl;
 
 	return 0;
 }
