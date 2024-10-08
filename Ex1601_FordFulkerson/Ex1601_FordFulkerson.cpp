77108edb36fc498680474f9f7c5b0a5292445944
nansu0425 <nansu0425@gmail.com>
2024-10-08 18:55:42 +0900

    Ex1601 완료

--- Ex1601_FordFulkerson/Ex1601_FordFulkerson.cpp
+++ Ex1601_FordFulkerson/Ex1601_FordFulkerson.cpp
@@ -12,179 +12,221 @@ using namespace std;
 class FlowEdge
 {
 public:
-	int v = -1;
-	int w = -1;
-	double capacity = 0.0; // 최대 용량
-	double flow = 0.0;     // 현재 유량
+	FlowEdge() = default;
 
-	FlowEdge()
-		:v(-1), w(-1), capacity(0.0), flow(0.0)
+	FlowEdge(int from, int to, double capacity)
+		: m_from(from)
+		, m_to(to)
+		, m_capacity(capacity)
 	{}
 
-	FlowEdge(int v, int w, double capacity)
-		: v(v), w(w), capacity(capacity), flow(0.0)
+	FlowEdge(int from, int to, double capacity, double flow)
+		: m_from(from)
+		, m_to(to)
+		, m_capacity(capacity)
+		, m_flow(flow)
 	{}
 
-	FlowEdge(int v, int w, double capacity, double flow)
-		: v(v), w(w), capacity(capacity), flow(flow)
-	{}
+	int GetFrom() const { return m_from; }
+	int GetTo() const { return m_to; }
+	double GetCapacity() const { return m_capacity; }
+	double GetFlow() const { return m_flow; }
 
-	int From() { return v; }
-	int To() { return w; }
-	double Capacity() { return capacity; }
-	double Flow() { return flow; }
-	int Other(int vertex)
+	int GetOther(int vertex) const
 	{
-		if (vertex == v) return w;
-		else if (vertex == w) return v;
+		if (vertex == m_from) return m_to;
+		else if (vertex == m_to) return m_from;
 		else {
 			assert(false);
 			return -1;
 		}
 	}
 
-	double ResidualCapacityTo(int vertex)
+	double GetResidualCapacityTo(int vertex) const
 	{
-		if (vertex == v) return flow;
-		else if (vertex == w) return capacity - flow;
+		if (vertex == m_from) return m_flow;
+		else if (vertex == m_to) return m_capacity - m_flow;
 
 		assert(false);
 
 		return 0.0;
 	}
 
-	void AddResidualFlowTo(int vertex, double delta)
+	void UpdateFlowTo(int vertex, double delta)
 	{
-		if (vertex == v) flow -= delta;
-		else if (vertex == w) flow += delta;
+		if (vertex == m_from) m_flow -= delta;
+		else if (vertex == m_to) m_flow += delta;
 		else assert(false);
 	}
 
-	void Print()
+	void Print() const
 	{
-		cout << v << "->" << w << " " << flow << "/" << capacity << endl;
+		cout << m_from << "->" << m_to << " " << m_flow << "/" << m_capacity << endl;
 	}
 
-	int Either() { return v; }
+private:
+	int		m_from = -1;
+	int		m_to = -1;
+	double	m_capacity = 0.0; // 최대 용량
+	double	m_flow = 0.0;     // 현재 유량
 };
 
 class FlowNetwork
 {
 public:
-	int V;
-	int E;
-	vector<list<FlowEdge*>> adj;
-
-	FlowNetwork(int V)
-		: V(V), E(0), adj(V)
+	FlowNetwork(int numVertices)
+		: m_numVertices(numVertices)
+		, m_numEdges(0)
+		, m_edgesTable(numVertices)
 	{}
 
-	void AddEdge(FlowEdge e)
+	void AddEdge(const FlowEdge& e)
 	{
-		FlowEdge* newEdge = new FlowEdge(e);
-		int v = e.Either();
-		int w = e.Other(v);
-		adj[v].push_back(newEdge); // v의 out_neighbor
-		adj[w].push_back(newEdge); // w의 in_neighbor
+		FlowEdge* pNewEdge = new FlowEdge(e);
+		int from = e.GetFrom();
+		int to = e.GetOther(from);
+
+		m_edgesTable[from].push_back(pNewEdge); // edge of out_neighbor
+		m_edgesTable[to].push_back(pNewEdge); // edge of in_neighbor
 	}
 
-	list<FlowEdge*>& Adj(int v)
+	int GetNumVertices() const { return m_numVertices; }
+
+	const list<FlowEdge*>& GetEdges(int vertex) const
 	{
-		return adj[v];
+		return m_edgesTable[vertex];
 	}
 
-	vector<FlowEdge*> AllEdges() {
-		vector<FlowEdge*> b;
-		for (int v = 0; v < V; v++)
-			for (auto& e : this->Adj(v))
-				if (e->Other(v) > v)
-					b.push_back(e);
-		return b;
+	vector<FlowEdge*> GetAllEdges() 
+	{
+		vector<FlowEdge*> allEdges;
+		allEdges.reserve(m_numEdges / 2);
+
+		for (int vertex = 0; vertex < m_numVertices; vertex++)
+		{
+			for (const auto& edge : GetEdges(vertex))
+			{
+				// 이미 추가한 edge인지 검사하여 중복 추가 방지
+				if (edge->GetOther(vertex) > vertex)
+				{
+					allEdges.push_back(edge);
+				}
+			}
+		}
+		return allEdges;
 	}
+
+private:
+	int						m_numVertices = 0;
+	int						m_numEdges = 0;
+	vector<list<FlowEdge*>> m_edgesTable;
 };
 
 class FordFulkerson
 {
 public:
-	vector<bool> marked;
-	vector<FlowEdge*> prev;
-	double value;
-
-	FordFulkerson(FlowNetwork& g, int s, int t)
-		: marked(g.V), prev(g.V), value(0.0)
+	FordFulkerson(FlowNetwork& flowNet, int source, int sink)
+		: m_scheduledTable(flowNet.GetNumVertices())
+		, m_prevTable(flowNet.GetNumVertices())
 	{
-		while (HasAugmentingPath(g, s, t))
+		while (IsAugmentingPath(flowNet, source, sink) == true)
 		{
-			// 힌트: Other(), ResidualCapacityTo(), AddResidualFlowTo() 사용
-			// TODO:
+			// 힌트: Other(), ResidualCapacityTo(), UpdateFlowTo() 사용
+			double minResidualCapacity = std::numeric_limits<double>::infinity();
+			FlowEdge* pPrevEdge = m_prevTable[sink];
 
-			Print(g);
+			// 최소 잔여 유량을 찾는다
+			while (pPrevEdge != nullptr)
+			{
+				minResidualCapacity = min(minResidualCapacity,
+										  pPrevEdge->GetResidualCapacityTo(pPrevEdge->GetTo()));
+				pPrevEdge = m_prevTable[pPrevEdge->GetFrom()];
+			}
 
-			break; // TODO: 삭제
+			pPrevEdge = m_prevTable[sink];
+
+			// 경로의 유량을 최소 잔여 유량만큼 증가시킨다
+			while (pPrevEdge != nullptr)
+			{
+				pPrevEdge->UpdateFlowTo(pPrevEdge->GetTo(), minResidualCapacity);
+				pPrevEdge = m_prevTable[pPrevEdge->GetFrom()];
+			}
+
+			m_maxFlow += minResidualCapacity;
+
+			Print(flowNet);
 		}
 	}
 
-	bool HasAugmentingPath(FlowNetwork& g, int s, int t)
+	bool IsAugmentingPath(const FlowNetwork& flowNet, int source, int sink)
 	{
-		fill(marked.begin(), marked.end(), false);
+		fill(m_scheduledTable.begin(), m_scheduledTable.end(), false);
 
-		queue<int> q; // BFS
+		queue<int> visitScheduler; // BFS
 
-		marked[s] = true;
-		q.push(s);
+		m_scheduledTable[source] = true;
+		visitScheduler.push(source);
 
-		while (!q.empty())
+		while (visitScheduler.empty() == false)
 		{
-			int v = q.front();
-			q.pop();
+			int visitedVertex = visitScheduler.front();
+			visitScheduler.pop();
 
-			for (FlowEdge* e : g.Adj(v))
+			for (FlowEdge* pNeighborEdge : flowNet.GetEdges(visitedVertex))
 			{
-				int w = e->Other(v);
-				if (!marked[w] && e->ResidualCapacityTo(w) > 0) // <- TODO: BFS와의 차이 확인
+				int neighborVertex = pNeighborEdge->GetOther(visitedVertex);
+
+				if ((m_scheduledTable[neighborVertex] == false) && 
+					(pNeighborEdge->GetResidualCapacityTo(neighborVertex) > 0)) // <- TODO: BFS와의 차이 확인
 				{
-					prev[w] = e;
-					marked[w] = true;
-					q.push(w);
+					m_prevTable[neighborVertex] = pNeighborEdge;
+					m_scheduledTable[neighborVertex] = true;
+					visitScheduler.push(neighborVertex);
 				}
 			}
 		}
 
-		return marked[t];
+		return m_scheduledTable[sink];
 	}
 
 	void Print(FlowNetwork& g)
 	{
 		cout << "Result:" << endl;
-		for (int v = 0; v < g.V; v++)
-			for (FlowEdge* e : g.Adj(v))
-				if (v == e->From())
+		for (int v = 0; v < g.GetNumVertices(); v++)
+			for (FlowEdge* e : g.GetEdges(v))
+				if (v == e->GetFrom())
 					e->Print();
-		cout << value << endl;
+		cout << m_maxFlow << endl;
 	}
+
+private:
+	vector<bool>			m_scheduledTable;
+	vector<FlowEdge*>		m_prevTable;
+	double					m_maxFlow = 0;
 };
 
 int main()
 {
 	// 강의 영상에 나오는 예시
-	FlowNetwork g(4);
+	/*FlowNetwork flowNet(4);
+
 	for (const auto& e : vector<FlowEdge>{ {0, 1, 4.0}, {0, 2, 8.0}, {1, 2, 6.0}, {1, 3, 2.0}, {2, 3, 3.0} })
 	{
-		g.AddEdge(e);
-	}
+		flowNet.AddEdge(e);
+	}*/
 
-	//FlowNetwork g(6); // CLRS 예제
-	//g.AddEdge({ 0, 1, 16.0 });
-	//g.AddEdge({ 0, 2, 13.0 });
-	//g.AddEdge({ 1, 3, 12.0 });
-	//g.AddEdge({ 2, 1, 4.0 });
-	//g.AddEdge({ 2, 4, 14.0 });
-	//g.AddEdge({ 3, 2, 9.0 });
-	//g.AddEdge({ 3, 5, 20.0 });
-	//g.AddEdge({ 4, 3, 7.0 });
-	//g.AddEdge({ 4, 5, 4.0 });
+	FlowNetwork flowNet(6); // CLRS 예제
+	flowNet.AddEdge({ 0, 1, 16.0 });
+	flowNet.AddEdge({ 0, 2, 13.0 });
+	flowNet.AddEdge({ 1, 3, 12.0 });
+	flowNet.AddEdge({ 2, 1, 4.0 });
+	flowNet.AddEdge({ 2, 4, 14.0 });
+	flowNet.AddEdge({ 3, 2, 9.0 });
+	flowNet.AddEdge({ 3, 5, 20.0 });
+	flowNet.AddEdge({ 4, 3, 7.0 });
+	flowNet.AddEdge({ 4, 5, 4.0 });
 
-	FordFulkerson f(g, 0, g.V - 1);
+	FordFulkerson f(flowNet, 0, flowNet.GetNumVertices() - 1);
 
 	return 0;
 }
